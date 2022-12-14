3c819e5f7855d79d361aaf1091717a752e10bef9
Boydem <noyemdahan@gmail.com>
2022-12-14 12:59:21 +0200

    added memes

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -7,7 +7,7 @@ function onInit() {
 function renderGallery() {
     const elGallery = document.querySelector('.meme-gallery')
     let imgStrHTML = ``
-    for (let i = 1; i <= 4; i++) {
+    for (let i = 1; i <= 17; i++) {
         imgStrHTML += `<img onclick="onImageClick(this)" src="images/meme-imgs (square)/${i}.jpg" alt="">`
     }
     elGallery.innerHTML = imgStrHTML
