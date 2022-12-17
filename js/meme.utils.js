07da80b4e9ed1844a879d785b222e1fa7b343956
Boydem <noyemdahan@gmail.com>
2022-12-17 23:26:05 +0200

    fixing nav

--- js/meme.utils.js
+++ js/meme.utils.js
@@ -8,15 +8,16 @@ function measureLineSizes(line) {
     }
 }
 
-function hideEditor(linkTo) {
+function hideEditor(linkTo = 'gallery') {
+    const elNavlinks = Array.from(document.querySelectorAll('.nav-link'))
+    elNavlinks.find(elLink => elLink.classList.contains('active')).classList.remove('active')
     if (gIsSavedEditor) {
         gActualMemeImg.style.display = 'none'
         gIsSavedEditor = false
+        elNavlinks.find(elLink => elLink.dataset.trans === 'saved').classList.add('active')
+    } else {
+        elNavlinks.find(elLink => elLink.dataset.trans === linkTo).classList.add('active')
     }
-    const elNavlinks = Array.from(document.querySelectorAll('.nav-link'))
-    elNavlinks.find(elLink => elLink.classList.contains('active')).classList.remove('active')
-    elNavlinks.find(elLink => elLink.dataset.trans === linkTo).classList.add('active')
-
     const elEditor = document.querySelector('.meme-editor')
     elEditor.classList.remove('show')
     resetLines()
